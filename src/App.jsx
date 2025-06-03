c51854f0fae385cce8f95a198ae0b0e15b1fd131
hossam6854 <150062047+hossam6854@users.noreply.github.com>
2025-06-03 16:15:58 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
-  import Blog from "./pages/Blog";
-  import Footer from "./components/Footer";
+import Blog from "./pages/Blog";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div className="main-content"> {/* يمنع التداخل مع النافبار */}
+      <ScrollToTop />
+      <div className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
